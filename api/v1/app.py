698e627ea49665d5968dc92860188e94b8c22311
youssef <youssefelghamour@gmail.com>
2024-03-29 09:10:42 -0700

    add a custom 404 error handler in api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''API app module that return the status of your API'''
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def tear_down(exception):
     'close the storage at the end of each request'
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ returns a JSON-formatted 404 status code response """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
