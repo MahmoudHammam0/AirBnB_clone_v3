ee963df138ded6b8a8cfd909d2d64e4127c9e03c
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-29 05:38:05 -0700

    3. Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
@@ -15,5 +16,7 @@ def tear_down(exception):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host, int(port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
     'return a JSON status representation'
     return jsonify({'status': 'OK'})
