830942d3ef63a72e7680fc05dcbef4214b99cd7b
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-30 02:47:28 -0700

    9. User

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -52,9 +52,8 @@ def create_user():
         abort(400, description="Missing email")
     if "password" not in request.json:
         abort(400, description="Missing password")
-    new_user = User()
-    for key, value in request.get_json().items():
-        setattr(new_user, key, value)
+    data = request.get_json()
+    new_user = User(**data)
     storage.new(new_user)
     storage.save()
     return jsonify(new_user.to_dict()), 201
