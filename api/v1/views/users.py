b409029ed1f18efcd8000aad1ee51f45b366ff61
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-04-01 03:34:40 -0700

    3. Status of your API

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 '''create view for User objects that handles default RESTFul API actions'''
-from models import storage
-from models.user import User
 from api.v1.views import app_views
+from models.user import User
+from models import storage
 from flask import jsonify, abort, request, make_response
 
 
